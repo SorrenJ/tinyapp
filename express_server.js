c95a46ff9ffa203407d1c77734723bb76f5e3bdf
SorrenJ <soalexjao@gmail.com>
2024-05-03 00:06:11 -0700

    POST request handling and generateRandomString fucntion implementation

--- express_server.js
+++ express_server.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {
+
+  let strOutput = "";
+  let lettersAndNumArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  for (let i = 0; i < 6; i++) {
+    let randomLetter = lettersAndNumArr[Math.floor(Math.random() * 61)];
+    strOutput += randomLetter;
+  }
+
+  return strOutput;
+}
 
 // This tells the Express app to use EJS as its templating engine.
 app.set("view engine", "ejs");
@@ -10,6 +22,17 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+
+
+// Handles POST request. Middleware, parses incoming requests with URL-encoded payloads
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -42,6 +65,7 @@ app.get("/hello", (req, res) => {
 // for fetch ReferenceError: a is not defined
 
 
+
 // render urls_index page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -54,8 +78,10 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
-
+// render urls_new page
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 
 
