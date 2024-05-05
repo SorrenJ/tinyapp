c8d4eb42c7740fac931a65831e98d76524641646
SorrenJ <soalexjao@gmail.com>
2024-05-05 16:51:20 -0700

    Basic permission features

--- express_server.js
+++ express_server.js
@@ -4,7 +4,6 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 function generateRandomString() {
-
   let strOutput = "";
   let lettersAndNumArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
@@ -16,9 +15,20 @@ function generateRandomString() {
   return strOutput;
 }
 
+const urlsForUser = function(id) {
+  let outputObj = {};
+
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      outputObj[shortURL] = urlDatabase[shortURL];
+    }
+  }
+
+  return outputObj;
+};
+
 // This tells the Express app to use EJS as its templating engine.
 app.set("view engine", "ejs");
-
 
 app.use(cookieParser());
 
@@ -29,8 +39,8 @@ app.use(express.urlencoded({ extended: true }));
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
-};
 
+};
 
 const users = {
   userRandomID: {
@@ -58,212 +68,180 @@ const findUserByEmail = (email) => {
   return null;
 };
 
+// Middleware to check if user is logged in
+const requireLogin = (req, res, next) => {
+  if (!req.cookies["user_id"]) {
+    return res.redirect("/login");
+  }
+  next();
+};
 
-
-
-
-
-// GET /login endpoint that responds with this new login form template
-app.get("/login", (req, res) => {
-  const templateVars = {
-    userInfo: users[req.cookies["user_id"]]
-  };
-  if(req.cookies["user_id"]){
+// Middleware to check if user is logged in and redirect from /register and /login
+const redirectToUrlsIfLoggedIn = (req, res, next) => {
+  if (req.cookies["user_id"]) {
     return res.redirect("/urls");
   }
+  next();
+};
 
+// 2. If someone is not logged in, redirects to the /login page: 
+// Middleware to check if user is not logged in and redirect from /urls/new
+const redirectToLoginIfNotLoggedIn = (req, res, next) => {
+  if (!req.cookies["user_id"]) {
+    return res.redirect("/login");
+  }
+  next();
+};
+// end of 2
+
+// 1. If someone is logged in and visits /register or /login pages, they will be redirected to /urls:
+
+// GET /register endpoint
+
+app.get("/register", redirectToUrlsIfLoggedIn, (req, res) => {
+  const templateVars = { userInfo: users[req.cookies["user_id"]] };
+  res.render("register", templateVars);
+});
+
+// GET /login endpoint
+app.get("/login", redirectToUrlsIfLoggedIn, (req, res) => {
+  const templateVars = { userInfo: users[req.cookies["user_id"]] };
   res.render("login", templateVars);
 });
 
 
-// Login route
-app.post("/login", (req, res) => {
-  const email = req.body.email; // from the input form
-  
-  const user = findUserByEmail(req.body.email);
+// end of 1
 
-  if (!user) {
-    return res.status(403).send('email cannot be found');
-  }
-
-  if (user) {
-    if (user.password !== req.body.password) {
-      return res.status(403).send('incorrect password!');
-    }
-}
-  
-  
-  res.cookie("user_id", findUserByEmail(email));
-  res.redirect("/urls/");
+// 3. If someone is not logged in and visits /urls/new, they will be redirected to /login:
+// GET /urls/new endpoint
+app.get("/urls/new", redirectToLoginIfNotLoggedIn, (req, res) => {
+  const templateVars = { userInfo: users[req.cookies["user_id"]] };
+  res.render("urls_new", templateVars);
 });
+// end of 3
 
-
-
-
-// Logout route
-app.post("/logout", (req, res) => {
-  res.clearCookie("user_id"); // clears the submitted user cookie
-  res.redirect("/urls/");
-});
-
-
-
-
-
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-// Output for http://localhost:8080/urls.json
-// {"b2xVn2":"http://www.lighthouselabs.ca","9sm5xK":"http://www.google.com"}
-
-
-app.get("/u/:id", (req, res) => {
-  let shortId = req.params.id;
-  const longURL = urlDatabase[shortId];
-  
-  if (!longURL) {
-    return res.status(404).send('shortened URL does not exist');
-  }
-
-  
-  res.redirect(longURL);
-});
-
-
-
-
-
-// render urls_index page
+// GET /urls endpoint
 app.get("/urls", (req, res) => {
   const templateVars = {
-    urls: urlDatabase,
-    userInfo: users[req.cookies["user_id"]] // displays the username
+    urls: urlsForUser(req.cookies["user_id"]),
+    userInfo: users[req.cookies["user_id"]], // displays the username
   };
+
+  // Checks if for registered user can view URLS
+  // if (!req.cookies["user_id"]) {
+  //   return res.status(401).send('Cannot view "My URLs", you are not logged in');
+  // }
+
   res.render("urls_index", templateVars);
 });
 
 
-
-
-
-
+// 4. If someone makes a POST request to /urls and they are not logged in, they should see a relevant error message:
+// POST /urls endpoint
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
- // res.send("Ok"); // Respond with 'Ok' (we will replace this)
-
-
-  let shortURL = generateRandomString();
   if (!req.cookies["user_id"]) {
-    return res.status(401).send('you are not logged in');
+    return res.status(401).send('You are not logged in');
   }
-
-  
-  urlDatabase[shortURL] = req.body.longURL; // assign user-inputted longURL to a generated shortURL
-
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = { longURL: req.body.longURL, userID: req.cookies["user_id"] };
   const templateVars = { userInfo: users[req.cookies["user_id"]] };
-
   res.redirect(`/urls/${shortURL}`);
-
-
-
-
-
 });
+// end of 4
 
-
-
-
-
-// route to delete url
-app.post("/urls/:id/delete", (req, res) => {
-  const id = req.params.id;
-
-  delete urlDatabase[id];
-
-  res.redirect("/urls/");
-});
-
-// route to register
-app.get("/register", (req, res) => {
-  const templateVars = { userInfo: users[req.cookies["user_id"]] };
-  if(req.cookies["user_id"]){
-    return res.redirect("/urls");
+// 5. If someone makes a request for a url that doesn't exist (no url with provided id in our database), they should see a relevant error message:
+// 6. Every user should be able to visit /u/:id whether they are logged in or not:
+// GET /u/:id endpoint
+app.get("/u/:id", (req, res) => {
+  let shortId = req.params.id;
+  const longURL = urlDatabase[shortId];
+  if (!longURL) {
+    return res.status(404).send('Shortened URL does not exist');
   }
-  res.render("register", templateVars);
+  res.redirect(longURL);
 });
+// end of 5 and 6
 
-// registration handler
-app.post("/register", (req, res) => {
-  let userObj = {};
-  let userID = generateRandomString();
+// GET /urls/:id endpoint
+app.get("/urls/:id", requireLogin, (req, res) => {
+  const specificURL = urlsForUser(req.cookies["user_id"]);
 
-  
-  if (req.body.email === "" || req.body.password === "") {
-    
-    // if user has empty email/password
-    return res.status(400).send('cannot have an empty email or password');
+  if (!specificURL[req.params.id]) {
+    return res.status(401).send("Cannot access these URLs!");
   }
 
-  const user = findUserByEmail(req.body.email);
-
-  // if user already exists
-  if (user) {
-    return res.status(400).send('Email already in use');
-  }
-  
-  
-  
-  
-  
-  
-  res.cookie("user_id", userID);
-
-  userObj.id = userID;
-  userObj.email = req.body.email;
-  userObj.password = req.body.password;
-  users[userID] = userObj;
-  console.log(users);
-  res.redirect('/urls');
-});
-
-// render urls_show page
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], userInfo: users[req.cookies["user_id"]] }; // displays the username
-  
-
-  
-  
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id].longURL,
+    userInfo: users[req.cookies["user_id"]],
+  };
   res.render("urls_show", templateVars);
 });
 
-// render urls_new page
-app.get("/urls/new", (req, res) => {
-  const templateVars = { userInfo: users[req.cookies["user_id"]] };
- 
-  if (!req.cookies["user_id"]) {
-    return res.redirect("/login");
-  }
- 
-  res.render("urls_new");
-});
-
-// route to update url
-app.post("/urls/:id", (req, res) => {
-  let shortId = req.params.id; // if the info is coming from the URL
-  let longURL = req.body.longURL; // if the info is coming from the input form
-
-  urlDatabase[shortId] = longURL;
-
+// POST /urls/:id endpoint
+app.post("/urls/:id", requireLogin, (req, res) => {
+  let shortId = req.params.id;
+  let longURL = req.body.longURL;
+  urlDatabase[shortId] = { longURL: longURL, userID: req.cookies["user_id"] };
   res.redirect("/urls/");
 });
 
+// POST /urls/:id/delete endpoint
+app.post("/urls/:id/delete", requireLogin, (req, res) => {
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect("/urls/");
+});
 
+// POST /logout endpoint
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id"); // clears the submitted user cookie
+  res.redirect("/login");
+});
 
+// POST /login endpoint
+app.post("/login", (req, res) => {
+  const email = req.body.email; // from the input form
+  const user = findUserByEmail(req.body.email);
+
+  if (!user) {
+    return res.status(403).send('Email cannot be found');
+  }
+
+  if (user.password !== req.body.password) {
+    return res.status(403).send('Incorrect password!');
+  }
+
+  res.cookie("user_id", user.id);
+  res.redirect("/urls/");
+});
+
+// POST /register endpoint
+app.post("/register", (req, res) => {
+  if (req.body.email === "" || req.body.password === "") {
+    return res.status(400).send('Cannot have an empty email or password');
+  }
+
+  const user = findUserByEmail(req.body.email);
+  if (user) {
+    return res.status(400).send('Email already in use');
+  }
+
+  let userID = generateRandomString();
+  res.cookie("user_id", userID);
+  users[userID] = { id: userID, email: req.body.email, password: req.body.password };
+  res.redirect('/urls');
+});
+
+// Root endpoint
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+// GET /urls.json endpoint
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
